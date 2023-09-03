521861dd56c767be9fe18aa2e6428ce543788e67
verhowyna <verhowyna@ukr.net>
2023-09-03 19:47:19 +0300

    sunday-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const markup = galleryItems.map(item => `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`).join('');
+galleryList.insertAdjacentHTML("beforeend", markup);
+
+galleryList.addEventListener("click", onClick)
+
+function onClick(evt) {
+    if (evt.target === evt.currentTarget) {
+        return;
+    }
+
+    evt.preventDefault();
+    const instance = basicLightbox.create(`<img src="${evt.target.dataset.source}" alt="${evt.target.alt}" />`);
+    instance.show();
+}
