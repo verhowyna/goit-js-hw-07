fc9e19f6dd6c72e2b9872d47feac0e8eadda43d7
verhowyna <verhowyna@ukr.net>
2023-09-17 12:06:05 +0300

    new

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,7 +1,5 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const galleryList = document.querySelector('.gallery');
-
 const markup = galleryItems.map(({ preview, original, description }) => `<li class="gallery__item">
    <a class="gallery__link" href="${original}">
       <img class="gallery__image" src="${preview}" alt="${description}" />
